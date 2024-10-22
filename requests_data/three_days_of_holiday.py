b1911ab3b7f9e5990f9a15a19388cb6906fc1603
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-22 19:27:38 +0300

    Done 2024.20.18 >> ДЗ: Выведите три ближайших праздника и через сколько дней они будут.

--- requests_data/three_days_of_holiday.py
+++ requests_data/three_days_of_holiday.py
@@ -1,13 +1,14 @@
 # Выведите три ближайших праздника и через сколько дней они будут.
 
-import requests                       # Импорт необходимого модуля для отправки HTTP-запросов
-from datetime import datetime, date   # Импорт классов datetime и date из модуля datetime для работы с датами и временем
+import requests                # Импорт необходимого модуля для отправки HTTP-запросов
+from datetime import datetime  # Импорт классов datetime из модуля datetime для работы с датами и временем
 
 # Определение типов для словаря праздников и списка праздников:
 DICT_HOLIDAY = dict[str, str]      # Тип словаря для хранения информации о празднике
 LIST_HOLIDAY = list[DICT_HOLIDAY]  # Тип списка для хранения словарей праздников
 
-CURRENT_DATA: date = datetime.now().date() # Получение текущей даты
+CURRENT_DATA: datetime.date = datetime.now().date() # Получение текущей даты
+
 
 def get_list_of_available_holiday() -> LIST_HOLIDAY:
     """
@@ -27,20 +28,22 @@ def get_dict_of_available_three_days_of_holiday(holidays: LIST_HOLIDAY) -> DICT_
     holidays (LIST_HOLIDAY): Список всех доступных праздников.
     Возвращает словарь, где ключ - название праздника, значение - информация о празднике.
     """
+    print(f"Текущая дата: {CURRENT_DATA}")
+
     result: DICT_HOLIDAY = {}  # Инициализируем пустой словарь для хранения результатов
     count: int = 0  # Счетчик для отслеживания количества найденных праздников
 
     # Проходим по каждому празднику в списке
     for holiday in holidays:
         # Преобразуем строку даты в объект datetime и извлекаем только дату
-        holiday_date: date = datetime.strptime(holiday['date'], '%Y-%m-%d').date()
+        holiday_date: datetime.date = datetime.strptime(holiday['date'], '%Y-%m-%d').date()
         # Вычисляем количество дней до праздника
         days_until: int = (holiday_date - CURRENT_DATA).days
 
         # Проверяем, не прошел ли праздник
         if days_until >= 0:
             # Добавляем праздник в результат, если он еще не прошел
-            result[holiday['name']] = f"Страна: {holiday['countryCode']} | через {days_until} дней ({holiday['date']})"
+            result[holiday['name']] = f"через {days_until} дней ({holiday['date']}) | Страна: {holiday['countryCode']}"
             count += 1
 
             # Если нашли три праздника, прерываем цикл
@@ -58,11 +61,9 @@ def main() -> None:
     list_of_holiday: LIST_HOLIDAY = get_list_of_available_holiday()
     dict_of_holiday: DICT_HOLIDAY = get_dict_of_available_three_days_of_holiday(list_of_holiday)
 
-    print(f"Текущая дата: {CURRENT_DATA}")
-
     print("Три ближайших праздника:")
     for name, details in dict_of_holiday.items():
-        print(f"- {name} | {details}")
+        print(f" - {name} | {details}")
 
 
 
