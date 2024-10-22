13d3aacba84e8b81e727d5ff020e8fc5030cf8d2
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-22 20:41:11 +0300

    Генерируем и сохраняем 10 000 строк случайных символов
    Реализованы функции для генерации случайных строк, создания строк текста и записи в файл.
    Генерируется 10 000 строк по 100 символов из букв и цифр ASCII.
    Затем сохраняется в файл 'src/generated_ten_thousand.txt'.

--- zip_text/generate_hundred_character.py
+++ zip_text/generate_hundred_character.py
@@ -1,19 +1,29 @@
 from random import randint, choices
 from string import ascii_letters, digits
 
-LINES: int = 10000
-COUNT: int = 100
+COUNT_LINES: int = 10000
+COUNT_SYMBOL: int = 100
+FILENAME = "src\generated_ten_thousand.txt"
 
 
-def generator_ten_thousand(symbol_count:int):
+def generated_string(symbol_count:int):
         symbol_random: str = ascii_letters + digits
         return "".join(choices(population=symbol_random, k=symbol_count))
 
 
+def generated_lines(count_symbol: int, lines: int) -> str:
+        return "".join(generated_string(count_symbol) + '\n' for _ in range(lines))
+
+
+def write_ten_thousand(filename: str, string: str):
+    with open(filename, 'w') as f:
+        f.writelines(string)
+
+
 def main() -> None:
-    for _ in range(LINES):
-        generators = generator_ten_thousand(COUNT)
-        print(generators)
+    result = generated_lines(COUNT_SYMBOL, COUNT_LINES)
+    print(result)
+    write_ten_thousand(FILENAME, result)
 
 
 if __name__ == "__main__":
