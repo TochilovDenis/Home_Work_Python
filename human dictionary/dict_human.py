0c71c05725154f790843584a0812050c7ba2162f
TochilovDenis <denis-tochilov@yandex.ru>
2024-11-02 23:45:28 +0300

    Добавлена функциональность работы с людьми
    Внесены следующие изменения:
    - Создан класс Human для представления информации о человеке
    - Реализована функциональность управления словарем для хранения данных
    - Пример: основная функция для ввода информации
    - Добавлено обработка дубликатов при добавлении новых записей

--- human dictionary/dict_human.py
+++ human dictionary/dict_human.py
@@ -1,12 +1,12 @@
-from datetime import datetime
+from datetime import datetime, date
 
 
 class Human:
-    def __init__(self, name: str, surname: str, patronymic: str, birth_date: datetime):
-        self.name = name
-        self.surname = surname
-        self.patronymic = patronymic
-        self.birth_date = birth_date
+    def __init__(self, name: str, surname: str, patronymic: str, birth_date: date):
+        self.name: str        = name
+        self.surname: str     = surname
+        self.patronymic: str  = patronymic
+        self.birth_date: date = birth_date
 
 
     def __str__(self):
@@ -16,6 +16,33 @@ class Human:
                 f"Дата рождения: {self.birth_date}")
 
 
-def add_human_to_dictionary(self, name: str, surname: str, patronymic: str, birthday: datetime):
-    add_result = dict()
+def add_person(dictionary: dict, person: str or Human) -> None:
+    full_name = f"{person.name} {person.surname} {person.patronymic}".strip()
 
+    if full_name in dictionary:
+        print(f"Человек с ФИО: {full_name} уже существует в словаре.")
+    else:
+        dictionary[full_name] = person
+        print(f"Человек: {full_name} успешно добавлен в словарь.")
+
+
+def main():
+    people_dict: dict     = dict()
+
+    while True:
+        name: str         = input("Введите имя: ").strip()
+        surname: str      = input("Введите фамилию: ").strip()
+        patronymic:str    = input("Введите отчество: ").strip()
+        date_birth: str   = input("Введите дату рождения (YYYY-MM-DD): ").strip()
+        format_date: str  = '%Y-%m-%d'
+        birth_date: date  = datetime.strptime(date_birth, format_date).date()
+        new_person: Human = Human(name, surname, patronymic, birth_date)
+        add_person(people_dict, new_person)
+
+        cont: str         = input("Добавить еще одну персону? (д/н): ").lower().strip()
+        if cont not in ['д', 'y']:
+            break
+
+
+if __name__ == "__main__":
+    main()
