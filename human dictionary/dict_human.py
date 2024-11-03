0759422e614723505ac680ef2c1bfb1c2f8d1db6
TochilovDenis <denis-tochilov@yandex.ru>
2024-11-03 22:06:54 +0300

    Обновление кода программы для генерации и хранения данных о людях
    1. Добавлены новые импорты
    2. Определены списки для имен, фамилий и отчеств
    3. Обновлена функция add_person:
      - Изменен тип входного параметра person
      - Добавлена проверка на существование человека в словаре
      - Возвращаемое значение изменилось на bool
      - Функция теперь принимает объект типа Human
    4. Добавлена новая функция generate_birth_date() для генерации случайной даты рождения
    5. Основная логика программы была переработана:
     - Теперь программа может генерировать случайных людей вместо ввода пользователем
     - Добавлена обработка ошибок при добавлении людей в словарь
     - Реализована логика для завершения работы программы после определенного количества ошибок подряд

--- human dictionary/dict_human.py
+++ human dictionary/dict_human.py
@@ -1,4 +1,10 @@
-from datetime import datetime, date
+from calendar import monthrange
+from datetime import date
+from random import randint, choice
+
+NAMES: list[str]       = ["Иван", "Петр", "Алексей", "Николай"]
+SURNAMES: list[str]    = ["Иванов", "Петров", "Сидоров", "Кузнецов"]
+PATRONYMICS: list[str] = ["Иванович", "Петрович", "Викторович", "Алексеевич"]
 
 
 class Human:
@@ -11,37 +17,60 @@ class Human:
 
     def __str__(self):
         return (f"Фамилия: {self.surname} | "
-                f"Имя: {self.name[0]} | "
-                f"Отчество: {self.patronymic[0]} | "
+                f"Имя: {self.name} | "
+                f"Отчество: {self.patronymic} | "
                 f"Дата рождения: {self.birth_date}")
 
 
-def add_person(dictionary: dict, person: str or Human) -> None:
-    full_name = f"{person.name} {person.surname} {person.patronymic}".strip()
+def add_person(dictionary: dict[str, Human], person: Human) -> bool:
+    full_name: str = f"{"".join(person.surname)} {"".join(person.name)} {"".join(person.patronymic)}"
 
     if full_name in dictionary:
         print(f"Человек с ФИО: {full_name} уже существует в словаре.")
+        return False
     else:
         dictionary[full_name] = person
-        print(f"Человек: {full_name} успешно добавлен в словарь.")
+        return True
+
+
+def generate_birth_date() -> date:
+    year = randint(1900, 2023)
+    month = randint(1, 12)
+    _, days_in_month = monthrange(year, month)
+    day = randint(1, days_in_month)
+    return date(year, month, day)
+
+
 
 
 def main():
-    people_dict: dict     = dict()
+    people_dict: dict[str, Human] = dict()
+    consecutive_errors: int = 0
+    total_additions: int = 0
+    total_errors: int = 0
+
 
     while True:
-        name: str         = input("Введите имя: ").strip()
-        surname: str      = input("Введите фамилию: ").strip()
-        patronymic:str    = input("Введите отчество: ").strip()
-        date_birth: str   = input("Введите дату рождения (YYYY-MM-DD): ").strip()
-        format_date: str  = '%Y-%m-%d'
-        birth_date: date  = datetime.strptime(date_birth, format_date).date()
-        new_person: Human = Human(name, surname, patronymic, birth_date)
-        add_person(people_dict, new_person)
+        birth_date: date  = generate_birth_date()
+        new_person: Human = Human(
+            choice(SURNAMES),
+            choice(NAMES),
+            choice(PATRONYMICS),
+            birth_date)
 
-        cont: str         = input("Добавить еще одну персону? (д/н): ").lower().strip()
-        if cont not in ['д', 'y']:
+        if add_person(people_dict, new_person):
+            total_additions += 1
+        else:
+            total_errors += 1
+            consecutive_errors += 1
+
+        if consecutive_errors >= 4:
+            print(f"Программа завершила работу после {consecutive_errors} ошибок подряд.")
             break
+
+    print(f"\nРезультаты:")
+    print(f"\nДобавлено людей: {total_additions}")
+    print(f"Всего ошибок добавления: {total_errors}")
 
 
 if __name__ == "__main__":
